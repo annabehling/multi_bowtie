0a253d62ec4634f91505f15132d772fbff7f42a8
Anna Martinson-Behling <anna.behling93@gmail.com>
2020-05-02 11:30:33 +1200

    updated preamble and included code to map with 0 mismatches

--- multi_bowtie.py
+++ multi_bowtie.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 """bowtie_multi.py automates the bowtie2 command line for multiple runs using different fastq files
-usage : bowtie_multi.py(bowtie_index, fq_directory)
+usage : bowtie_multi.py <bowtie_index> <fq_directory>
 the script takes a directory of fastq files and the stem name of a bowtie2 index,
 then, using subprocess it calls bowtie2 with the arguments generated from fastq_files() and sam_names()
 #bowtie_index : bowtie2 index stem name, e.g. G_a_db
@@ -18,10 +18,10 @@ def fastq_files(fq_directory):
 	fq_files = []
 	for file in os.listdir(fq_directory):
 		#print(file)
-		if file.endswith('.fastq.trimmed'):
+		if file.endswith('.fastq.trimmed.single'): #fastq file format following SolexaQA dynamictrim and lengthsort
 			fq_files.append(file)
 	if len(fq_files) == 0:
-		raise ValueError("No files with ending '.fastq.trimmed' found in dir")		
+		raise ValueError("No files with ending '.fastq.trimmed.single' found in dir")		
 	return(fq_files)
 
 def sam_names(fq_directory, bowtie_index):
@@ -47,7 +47,7 @@ if __name__ == '__main__':
 	sam = sam_names(args.fq_directory, args.bowtie_index)
 	loop_index = 0
 	for sam_value in sam:
-		subprocess.call(['bowtie2', '-x', args.bowtie_index, '-U', fastq[loop_index], '-S', sam[loop_index]])
+		subprocess.call(['bowtie2', '--score-min "C,0,-1"', '-x', args.bowtie_index, '-U', fastq[loop_index], '-S', sam[loop_index]]) #added in score min for 0 mismatches
 		print('Created file called {}'.format(sam[loop_index]) )
 		loop_index += 1 #so that it will go to the next value in the list
 
