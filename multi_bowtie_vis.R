02b87556f1e8b71503d3c78ad72614841d54b8ba
Anna Martinson-Behling <anna.behling93@gmail.com>
2020-08-19 16:09:45 +1200

    minor code fixes

--- multi_bowtie_vis.R
+++ multi_bowtie_vis.R
@@ -8,11 +8,11 @@ library(ggplot2)
 import_idxstats <- function(stats_dir){
   #stats_dir : path to directory containing Bowtie2 index stats tsv files
   fnames <- list.files(path = stats_dir,
-                       pattern = ".tsv",
+                       pattern = '.tsv',
                        full.names = TRUE) #get list of tsv files in directory
-  stats_tables <- lapply(fnames, read.table, sep = "\t") #read in list of tsv files
+  stats_tables <- lapply(fnames, read.table, sep = '\t') #read in list of tsv files
   basenames <- lapply(fnames, basename) #get just file name
-  base_stem <- lapply(basenames, strsplit, "[./]") #split on '.' to get the stem of the file name
+  base_stem <- lapply(basenames, strsplit, '[./]') #split on '.' to get the stem of the file name
   names(stats_tables) <- lapply(base_stem, function(x) lapply(x, '[[', 1)) #assumes no other . in file name
   stats_tables
 }
@@ -21,8 +21,8 @@ import_idxstats <- function(stats_dir){
 merge_tables <- function(hylite_exp_file, bowtie_idxstats){
   merged <- merge(x = hylite_exp_file, 
                   y = bowtie_idxstats,
-                  by.x = "GENE",
-                  by.y = "V1") #assumes hylite was run on the same gene file as was used in the stringent mapping
+                  by.x = 'GENE',
+                  by.y = 'V1') #assumes hylite was run on the same gene file as was used in the stringent mapping
   names(merged) <- paste0(bowtie_idxstats, '_m')
   merged
 }
@@ -30,7 +30,7 @@ merge_tables <- function(hylite_exp_file, bowtie_idxstats){
 ## example
 
 # read in the HyLiTE expression.txt file
-HH_p_exp <- read.table('HH_p_hylite.expression.txt', sep="\t", header=TRUE)
+HH_p_exp <- read.table('HH_p_hylite.expression.txt', sep = '\t', header = TRUE)
 
 # import the Bowtie2 index statistics
 idx_tabs <- import_idxstats('.')
