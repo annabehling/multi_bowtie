0600244e96db082b67d2675fdab6694609d34e5e
Anna Martinson-Behling <anna.behling93@gmail.com>
2020-08-20 12:29:45 +1200

    minor code fixes

--- igv_picker.R
+++ igv_picker.R
@@ -8,10 +8,10 @@ igv_picker <- function(exp_file, readsum_file){
   readsum_tab <- read.table(readsum_file, sep = '\t', header = TRUE) #read in the read.summary.txt file
   
   red_exp_tab <- exp_tab[ ,c(1, 4)] #give only the gene and HH rep1 columns from the expression table
-  red_readsum_tab <- readsum_tab[ ,c(1, 2, 4)] #give just the gene and parental columns (no +N) from the read summary table
+  red_readsum_tab <- readsum_tab[ ,c(1, 4, 2)] #give just the gene and parental columns (no +N) from the read summary table
   merged_tab <- merge(red_exp_tab, red_readsum_tab, by='GENE') #merge the reduced expression table with the read summary table
-  merged_tab$p_reads <- merged_tab[ ,4] + merged_tab[ ,3] #make a new column with total reads mapped to both parents
-  merged_tab$p1_prop <- (merged_tab[ ,4]/merged_tab$p_reads)*100 #make a new column with relative proportion of P1 reads
+  merged_tab$p_reads <- merged_tab[ ,3] + merged_tab[ ,4] #make a new column with total reads mapped to both parents
+  merged_tab$p1_prop <- (merged_tab[ ,3]/merged_tab$p_reads)*100 #make a new column with relative proportion of P1 reads
   
   candidate_genes <- merged_tab[(merged_tab[ ,2] >= 150) & (merged_tab[ ,2] <= 200), ] #give only genes with 150-200 reads mapped to HH
   candidate_genes <- candidate_genes[candidate_genes$p_reads >= 100, ] #give only genes with at least 100 reads assigned to parents
